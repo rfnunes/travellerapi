03bbc99640ce9ea9dc9423de9bacd8264dd18de6
Ricardo Nunes <Ricardo.Nunes@gls-group.com>
2023-08-25 12:18:33 +0100

    Add ID to traveller

--- src/main/java/com/travellerapi/dto/Traveller.java
+++ src/main/java/com/travellerapi/dto/Traveller.java
@@ -2,7 +2,8 @@ package com.travellerapi.dto;
 
 import java.util.Set;
 
-public record Traveller(String firstName,
+public record Traveller(Long id,
+                        String firstName,
                         String lastName,
                         String birthDate,
                         String emailAddress,
